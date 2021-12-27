7ca17760ff2992a1143830326978f1e16ac6d561
Arman19941113 <td19941113@gmail.com>
2021-12-27 21:06:37 +0800

    feat: update css

--- rollup.config.ts
+++ rollup.config.ts
@@ -3,6 +3,12 @@ import { nodeResolve } from '@rollup/plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
 import gzipPlugin from 'rollup-plugin-gzip'
 import replace from '@rollup/plugin-replace'
+import rollupPostcss from 'rollup-plugin-postcss'
+import postcssNested from 'postcss-nested'
+// @ts-ignore
+import postcssPresetEnv from 'postcss-preset-env'
+// @ts-ignore
+import cssnano from 'cssnano'
 
 export default [{
   input: 'src/index.ts',
@@ -33,5 +39,23 @@ export default [{
     typescript(),
     terser(),
     gzipPlugin(),
+  ],
+}, {
+  input: 'src/index.css',
+  output: {
+    file: 'npm/dist/index.css',
+    format: 'es',
+  },
+  plugins: [
+    rollupPostcss({
+      extract: true,
+      plugins: [
+        postcssNested,
+        postcssPresetEnv({
+          stage: 0,
+        }),
+        cssnano,
+      ],
+    }),
   ],
 }]
